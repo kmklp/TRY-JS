0720d7be5dfb6b6736aa0c07bd28d07ab912699e
Maxim Samsonyuk <112984222+kmklp@users.noreply.github.com>
2023-03-02 15:07:52 +0200

    //**

---
+++ js/repeta.1lesson/01-vars.js
@@ -0,0 +1,15 @@
+//Переменные и типы примитивов
+//const и let
+//логирование методом console.log
+//уникальность идентификаторов
+
+
+
+const age = 10;
+const totalPrice = 200.74;
+const userName = 'Chelsy';
+const messege = 'Добро пожаловать';
+const isOpen = true;
+const shouldConfirm = falshe;
+
+console.log(5);
