1223f4d018428856f63ebdd50c82d7b9acd5b638
Maxim Samsonyuk <112984222+kmklp@users.noreply.github.com>
2023-03-31 22:15:50 +0300

    МАССИВЫ

--- js/repeta2modul/01-arrays.basis.js
+++ js/repeta2modul/01-arrays.basis.js
@@ -31,7 +31,7 @@ for (const friend of friends) {
     console.log(friend);
 }*/
 
-    /*Задач
+                                                 /*Задач
 Посчитать общую суммму покупки в корзине
 
 const cart = [54, 28, 105, 70, 92, 17, 120];
@@ -59,7 +59,7 @@ for (let i = 0; i < cart.length; i += 1){
 }
 console.log(cart);
 */
-           //Задача 3 - напиши скрипт который подсчитывает сумму всех чётных чисел в массиве
+                               //Задача 3 - напиши скрипт который подсчитывает сумму всех чётных чисел в массиве
 //1 Переменная total
 //const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
 //let total = 0;
@@ -84,7 +84,7 @@ for (const number of numbers) {
 }
 console.log(`сумма всех чётных чисел =`, total);
 */
-            //Лучше делать логику от обратного с помошью continue
+                                              //Лучше делать логику от обратного с помошью continue
 /*
 for (const number of numbers) {
     if (number % 2 !== 0) {
@@ -97,13 +97,14 @@ for (const number of numbers) {
 console.log(`сумма всех чётных чисел =`, total);
 */
       //Задача на поиск в массиве обьекта, имени
-const logins = [`manDoge`, `kiwiDubst`, `polySkout`, `ajaxTen`];
-const loginToFind = `polySkout`;
 
+//const logins = [`manDoge`, `kiwiDubst`, `polySkout`, `ajaxTen`];
+//const loginToFind = `polySkout`;
+/*
 for (let i = 0; i < logins.length; i += 1) {
-    const logins = logins[i];
+    const login = logins[i];
 
-    if (logins === loginToFind) {
+    if ( login === loginToFind) {
         message = `обьект ${loginToFind} найден`;
         break;
     }
@@ -112,3 +113,128 @@ for (let i = 0; i < logins.length; i += 1) {
     
 }
 console.log(message);
+
+const message = logins.includes(loginToFind) //назівается абстракция
+? `Пользователь ${loginToFind} найден`
+: `Пользователь ${loginToFind} не найден`;
+
+console.log(message);
+*/
+//const numbers = [7, 18, 14, 24, 5, 85, 19];
+//let smallNumber = numbers[0];
+ /*
+for (number of numbers) {
+    if (number < smallNumber) {
+        smallNumber = number;
+    }
+}
+console.log(`Cамій маленький це ${smallNumber}`);
+*/
+
+                                                    // удаляем последний єлемент ,  с помощью slice
+     /*
+const logins = [`manDoge`, `kiwiDubst`, `polySkout`, `ajaxTen`,];
+//let string = ``;
+
+for (const login of logins) {
+    string += login + `,`;
+}
+string = string.slice(0, string.length - 1);
+console.log(string);
+// получится manDoge`, `kiwiDubst`, `polySkout`, `ajaxTen` - без запятой
+
+     //Но есть метод сшивания сторок join
+const string = logins.join(`, `);
+console.log(string);
+*/
+                                              //ЗАДАЧА С ПРИМЕНЕНИЕМ SPLIT- эта задача не получилась!!!!!!
+/*
+*Напиши скрипт который заменяет регистр каждого символа в строке на противоположный
+*Например б если строка `JfvaScript` то на выходе должна быть строка `jAVAsCRIPT`
+*/
+/*
+const string = 'JavaScript';
+const letters = string.split('');
+let inverterString = '';
+
+console.log(letters);
+
+for (const letter of letters) {
+    console.log(letter);
+
+    const isEqual = letter === letter.toLowerCase();
+    inverterString +=
+        letter === toLowerCase()
+        ? letter.toUpperCase()
+        : letter.toLowerCase();
+}
+console.log('inverterString:', inverterString);
+*/
+
+           //Метод работі с массиво - текстом вниз.разїединили по пробелу и сшили с помощью тире-
+/*
+const title = 'Top 10 benefits of React framework';
+
+const normalisedTitle = title.toLowerCase();
+console.log(normalisedTitle);
+
+const words = normalisedTitle.split(' ');
+console.log(words);
+
+const dash = words.join('-');
+console.log(dash);
+                                             //Ещё проще
+const dash1 = title.toLowerCase().split(' ').join('-');
+
+console.log(dash1);
+*/
+                    // Задача 2 напиши скрипт который считывает сумму двух массивов
+/*
+const array1 = [5, 10, 15, 20];
+const array2 = [10, 20, 30];
+let total = 0;
+
+const numbers = array1.concat(array2);
+
+for (const number of numbers) {
+    total += number;
+}
+console.log(total);
+*/
+
+            //Робота с картами
+const cards = [
+  "карточка - 1",
+  "карточка - 2",
+  "карточка - 3",
+  "карточка - 4",
+  "карточка - 5",
+];
+console.table(cards);
+//удаление по индексу - indexOf()
+/*
+const cardRemove = "карточка - 3";
+const index = cards.indexOf(cardRemove);
+console.log(index);
+
+cards.splice(index, 1);
+
+console.table(cards);
+*/
+               //вставляем новую карточку "карточка - 6" на третье место 
+               /*
+const cardInsert = "карточка - 6"; 
+const index = 3;
+
+cards.splice(index, 0, cardInsert);
+console.table(cards);
+*/
+                         //удаляем карточку 2 и ставим "карточку - 7";
+const cardUp = "карточку - 7";
+const cardRemove = "карточка - 2";
+const index = cards.indexOf(cardRemove);
+
+console.log(index);
+
+cards.splice(index, 1, cardUp);
+console.table(cards);
