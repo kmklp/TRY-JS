2965b3df0a74a1e708663a64b58d8cb33f90fdb6
Maxim Samsonyuk <112984222+kmklp@users.noreply.github.com>
2023-04-02 13:42:02 +0300

    **

---
+++ js/repeta2modul/02-functions.basis.js
@@ -0,0 +1,26 @@
+/*Функции
+- Функциональные выражения
+- Аргументы и параметры
+- Возврат значения*/
+/*
+const add = function (x, y) {
+    console.log(x);
+    console.log(y);
+    console.log('Выполняется функция add');
+
+    return x + y ;
+}
+const r1 = add(5, 30);
+console.log('r1:', r1);
+
+const r2 = add(10, 15);
+console.log('r2:', r2);
+
+const r3 = add(30, 50);
+console.log('r3 = ', r3);
+*/
+
+            //Функции
+const calculateTotalPrice = function (items) {
+    console.log('items внутри функции:', items);
+ }
